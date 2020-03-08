b39abd020c56732312e53662a6b89fda1bc41c98
TheGreasyMuffin <lbuckland0223@gmail.com>
2020-03-08 05:05:35 +0000

    added buttons to character page

--- assets/js/gamemanager.js
+++ assets/js/gamemanager.js
@@ -16,7 +16,7 @@ let GameManager = {
                 break;
         }
         let getInterface = document.querySelector(".character");
-        getInterface.innerHTML = "<img src='assets/images/" + classType.toLowerCase() + ".png' alt='Character img' class='character-image'><div><h2>" + classType + "</h2><p>Health: " + player.health + "</p><p>Speed: " + player.speed + "</p><p>Magic: " + player.magic + "</p><p>Stamina: " + player.stamina + "</p></div>";
+        getInterface.innerHTML = "<div><img src='assets/images/" + classType.toLowerCase() + ".png' alt='Character img' class='character-image'><h2>" + classType + "</h2><p>Health: " + player.health + "</p><p>Speed: " + player.speed + "</p><p>Magic: " + player.magic + "</p><p>Stamina: " + player.stamina + "</p></div><a href='" + classType.toLowerCase() + ".html'><button>Do You Choose The "+ classType +"</button><a href='index.html'><button>Choose Again?</button></a>"; 
     },
     setPreFight: function () {
 
